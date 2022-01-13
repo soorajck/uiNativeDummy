bea30894d135dde9a8ece2a600890a57788dc427
CK SOORAJ KUMAR <cksoorajkumar@StartXlabs.local>
2022-01-13 19:25:21 +0530

    third

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -2,20 +2,34 @@ import * as React from 'react';
 import {Image, StyleSheet, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {Home, Cart, Search, Favorite} from '../screens';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 
 function Routes() {
   //creating tab navidation
-  const Tab = createMaterialBottomTabNavigator();
+  const Tab = createBottomTabNavigator();
   //this file is only for the bottom tab navigation
+
+  const bottomNavigatorConfigs = {
+    screenOptions: {
+      showLabel: false,
+      tabBarStyle: {
+        height: 100,
+        paddingTop: 30,
+        backgroundColor: 'rgba(40, 49, 59, 0.83)',
+      },
+    },
+  };
 
   return (
     <NavigationContainer>
-      <Tab.Navigator barStyle={styles.bottomTab} labeled={false}>
+      <Tab.Navigator {...bottomNavigatorConfigs}>
         <Tab.Screen
           name="Home"
           component={Home}
           options={{
+            headerShown: false,
+            tabBarLabel: '',
             tabBarIcon: ({focused}) => {
               return <Image source={require('../../assets/images/home.png')} />;
             },
@@ -25,6 +39,8 @@ function Routes() {
           name="Search"
           component={Search}
           options={{
+            headerShown: false,
+            tabBarLabel: '',
             tabBarIcon: ({focused}) => {
               return (
                 <Image source={require('../../assets/images/search.png')} />
@@ -36,6 +52,8 @@ function Routes() {
           name="Favorite"
           component={Favorite}
           options={{
+            headerShown: false,
+            tabBarLabel: '',
             tabBarIcon: ({focused}) => {
               return <Image source={require('../../assets/images/fav.png')} />;
             },
@@ -45,6 +63,8 @@ function Routes() {
           name="Cart"
           component={Cart}
           options={{
+            headerShown: false,
+            tabBarLabel: '',
             tabBarIcon: ({focused}) => {
               return (
                 <Image
@@ -64,7 +84,8 @@ export default Routes;
 const styles = StyleSheet.create({
   bottomTab: {
     backgroundColor: 'rgba(40, 49, 59, 0.6)',
-    height: 100,
-    paddingTop: 20,
+    height: 90,
+    position: 'absolute',
+    paddingTop: 10,
   },
 });
